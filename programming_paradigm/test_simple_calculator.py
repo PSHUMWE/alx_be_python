0f567075c4765c1da070edc3ffe2db5518b7feb0
Pessah Shumwe <pessahshumwe@gmail.com>
2025-08-08 14:07:14 +0300

    Add SimpleCalculator class and unit tests

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -4,36 +4,33 @@ import unittest
 from simple_calculator import SimpleCalculator
 
 class TestSimpleCalculator(unittest.TestCase):
-
     def setUp(self):
-        """Set up the calculator instance before each test."""
         self.calc = SimpleCalculator()
 
-    def test_add(self):
-        self.assertEqual(self.calc.add(2, 3), 5)
-        self.assertEqual(self.calc.add(-1, -1), -2)
-        self.assertEqual(self.calc.add(-5, 10), 5)
+    def test_addition(self):
+        self.assertEqual(self.calc.add(1, 2), 3)
+        self.assertEqual(self.calc.add(-1, -2), -3)
+        self.assertEqual(self.calc.add(-5, 5), 0)
         self.assertEqual(self.calc.add(0, 0), 0)
 
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(5, 3), 2)
         self.assertEqual(self.calc.subtract(0, 5), -5)
         self.assertEqual(self.calc.subtract(-5, -5), 0)
 
     def test_multiply(self):
         self.assertEqual(self.calc.multiply(2, 3), 6)
-        self.assertEqual(self.calc.multiply(-1, 5), -5)
-        self.assertEqual(self.calc.multiply(0, 100), 0)
+        self.assertEqual(self.calc.multiply(-2, 3), -6)
+        self.assertEqual(self.calc.multiply(0, 10), 0)
 
     def test_divide(self):
         self.assertEqual(self.calc.divide(10, 2), 5)
-        self.assertEqual(self.calc.divide(5, 2), 2.5)
-        self.assertEqual(self.calc.divide(-10, -2), 5)
-        self.assertEqual(self.calc.divide(0, 1), 0)
+        self.assertEqual(self.calc.divide(7, 2), 3.5)
+        self.assertEqual(self.calc.divide(-6, -3), 2)
 
     def test_divide_by_zero(self):
-        self.assertIsNone(self.calc.divide(5, 0))
-        self.assertIsNone(self.calc.divide(0, 0))  # Should return None (division undefined)
+        self.assertIsNone(self.calc.divide(10, 0))
+        self.assertIsNone(self.calc.divide(0, 0))
 
 if __name__ == "__main__":
     unittest.main()
